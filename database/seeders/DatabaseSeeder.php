340ae152c698c7c5a44c12bdd9341a02029332f1
muhammadwarisi <mw.a.seven.one@gmail.com>
2025-10-07 09:10:14 +0700

    refactor DatabaseSeeder to properly call UserSeeder and TasksSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\UserSeeder;
+use Database\Seeders\TasksSeeder;
 use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +16,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-        UserSeeder::class;
-        TasksSeeder::class;
+        $this->call([
+            UserSeeder::class,
+            TasksSeeder::class,
+        ]);
 
     }
 }
